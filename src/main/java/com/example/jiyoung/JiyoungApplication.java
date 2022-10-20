4e5ca34d01bbc813394bc71e0517b48fe7175801
yangjiyoung <0112jyoung@naver.com>
2022-10-20 17:20:48 +0900

    init

---
+++ src/main/java/com/example/jiyoung/JiyoungApplication.java
@@ -0,0 +1,13 @@
+package com.example.jiyoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiyoungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiyoungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jiyoung/JiyoungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jiyoung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiyoungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
