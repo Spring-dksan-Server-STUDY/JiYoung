43ae806480c862a0027169e8deeb32293f2349a6
yangjiyoung <0112jyoung@naver.com>
2022-10-26 19:04:53 +0900

    week2 - post, get 설정

--- src/main/java/com/example/jiyoung/assignment2/BookingController.java
+++ src/main/java/com/example/jiyoung/assignment2/BookingController.java
@@ -1,12 +1,30 @@
 package com.example.jiyoung.assignment2;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.jiyoung.assignment2.model.Booking;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.io.Console;
+import java.util.List;
 
 @RestController
 @RequestMapping("booking")
+@RequiredArgsConstructor
 public class BookingController {
-    private EntityManager em;
+    private final EntityManager entityManager;
+
+    @GetMapping("/")
+    public List<Booking> getBooking(){
+        return entityManager.createQuery("select bookingInfo from Booking bookingInfo", Booking.class).getResultList();
+    }
+
+    @PostMapping("/add")
+    @Transactional
+    public String addBooking(@RequestBody final Booking bookingInfo){
+        entityManager.persist(bookingInfo);
+        System.out.println(bookingInfo.getBookingNum());
+        return bookingInfo.getBookingNum(); // 예약번호 보내기
+    }
 }
