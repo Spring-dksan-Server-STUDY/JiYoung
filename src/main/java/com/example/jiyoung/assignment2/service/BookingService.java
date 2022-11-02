17efbef66f5d17e74c767392695a91c356ca44a5
yangjiyoung <0112jyoung@naver.com>
2022-11-02 14:30:19 +0900

    week3 - service 구현

---
+++ src/main/java/com/example/jiyoung/assignment2/service/BookingService.java
@@ -0,0 +1,34 @@
+package com.example.jiyoung.assignment2.service;
+
+import com.example.jiyoung.assignment2.model.Booking;
+import com.example.jiyoung.assignment2.repository.BookingRepository;
+
+import java.util.List;
+
+public class BookingService implements BookingServiceInterface{
+    private final BookingRepository repository;
+
+    public BookingService(BookingRepository bookingRepository){
+        this.repository=bookingRepository;
+    }
+
+    @Override
+    public List<Booking> getBooking() {
+        return repository.getBooking();
+    }
+
+    @Override
+    public String addBooking(Booking booking) {
+        return repository.addBooking(booking);
+    }
+
+    @Override
+    public String changeSeat(String bookingNum, String seat) {
+        return repository.changeSeat(bookingNum, seat);
+    }
+
+    @Override
+    public String cancelBooking(String bookingNum) {
+        return repository.cancelBooking(bookingNum);
+    }
+}
